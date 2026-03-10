7d714253ebc941a82bf349017fb7ffba1ada7555
rajat-cr <rajat@coderroots.com>
2026-03-10 10:50:10 +0530

    Check Description
    Bottom Navigation

---
+++ app/src/main/java/com/coderroots/deepakcomposeclass/bottomnavigation/BottomNavActivity.kt
@@ -0,0 +1,184 @@
+package com.coderroots.deepakcomposeclass.bottomnavigation
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierInfo
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat.enableEdgeToEdge
+import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class BottomNavActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent{
+            BottomNavScreen()
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showSystemUi = true)
+@Composable
+fun BottomNavScreen(){
+
+    var navController = rememberNavController()
+    var selectedIndex by remember { mutableStateOf(0) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Dashboard",
+                        fontWeight = FontWeight.Bold)
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(R.color.holo_purple),
+                    titleContentColor = Color.White
+                )
+            )
+
+        },
+        bottomBar = {
+            BottomAppBar(Modifier.fillMaxWidth(), containerColor = Color.White){
+
+                Row(Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround) {
+                    Icon( if(selectedIndex == 0)
+                        Icons.Filled.Home
+                                else
+                        Icons.Outlined.Home,
+                        contentDescription = "",
+                        modifier = Modifier.size(30.dp).clickable{
+                            selectedIndex = 0
+                            navController.navigate("home"){
+                                popUpTo(navController.graph.startDestinationId){
+                                  saveState = true
+                                }
+                                restoreState = true
+                                launchSingleTop = true
+                            }
+                        } )
+                    Icon(if(selectedIndex == 1)
+                        Icons.Filled.AccountCircle
+                    else
+                        Icons.Outlined.AccountCircle,
+                        contentDescription = "",
+                        modifier = Modifier.size(30.dp).clickable{
+                            selectedIndex = 1
+                            navController.navigate("profile"){
+                                popUpTo(navController.graph.startDestinationId){
+                                    saveState = true
+                                }
+                                restoreState = true
+                                launchSingleTop = true
+                            }
+                        })
+
+                    Icon(if(selectedIndex == 2)
+                        Icons.Filled.Settings
+                    else
+                        Icons.Outlined.Settings,
+                        contentDescription = "",
+                        modifier = Modifier.size(30.dp).clickable{
+                            selectedIndex = 2
+                            navController.navigate("setting"){
+                                popUpTo(navController.graph.startDestinationId){
+                                    saveState = true
+                                }
+                                restoreState = true
+                                launchSingleTop = true
+                            }
+                        })
+                }
+            }
+        }
+    ) { innerPadding->
+        NavHost(
+            navController = navController,
+            startDestination = "home",
+            modifier = Modifier.fillMaxSize().padding(innerPadding)
+        ) {
+            composable("home") {
+                HomeScreen()
+            }
+            composable("profile") {
+                ProfileScreen()
+            }
+            composable("setting") {
+                SettingScreen()
+            }
+        }
+
+
+    }
+
+}
+
+@Composable
+fun HomeScreen(){
+    Box(Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+
+            Text("Home Screen")
+    }
+}
+
+@Composable
+fun ProfileScreen(){
+    Box(Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text("Profile, Screen")
+    }
+
+}
+
+@Composable
+fun SettingScreen(){
+    Box(Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center){
+        Text("Setting Screen")
+    }
+
+}
